2830e5b39017f52830e3a3021c8366468b541811
Kiea Kang <grtlinux@gmail.com>
2022-07-28 16:42:38 +0900

    create 1st

---
+++ KieaSwagger-01/src/main/java/org/tain/KieaSwagger01Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaSwagger01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaSwagger01Application.class, args);
+	}
+
+}
